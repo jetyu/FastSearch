1f35c5b766c5cf8815cf4ffcf3777562d62296ed
Jet <2314520+jetyu@users.noreply.github.com>
2026-09-17 08:12:52 +0800

    Refactor SVG icon and update news sources
    - Updated the all-search SVG icon to a simpler design featuring a magnifying glass and a forward arrow.
    - Removed several news sources from the configuration, retaining only the relevant ones, and added 今日头条 as a new source.

--- src/config/sites/news.js
+++ src/config/sites/news.js
@@ -5,29 +5,12 @@ export default [
     icon: 'https://www.google.com/favicon.ico'
   },
   {
-    nameZh: '百度新闻',
-    url: 'https://news.baidu.com/ns?word=%s&tn=news&from=news&cl=2&rn=20&ct=1',
-    icon: 'https://www.baidu.com/favicon.ico'
-  },
-  {
     nameZh: '凤凰新闻',
     url: 'https://so.ifeng.com/?q=%s&c=1'
   },
   {
-    nameZh: '人民网',
-    url: 'https://search.people.cn/s/?keyword=%s'
-  },
-  {
-    nameZh: '央视网',
-    url: 'https://search.cctv.com/search.php?qtext=%s&type=web'
-  },
-  {
-    nameZh: '澎湃新闻',
-    url: 'https://www.thepaper.cn/searchResult?id=%s'
-  },
-  {
-    nameZh: '中新网',
-    url: 'https://sou.chinanews.com.cn/search.do?q=%s'
+    nameZh: '今日头条',
+    url: 'https://www.toutiao.com/search/?keyword=%s'
   },
   {
     nameZh: 'CNN',
@@ -36,9 +19,5 @@ export default [
   {
     nameZh: 'BBC',
     url: 'https://www.bbc.co.uk/search?q=%s'
-  },
-  {
-    nameZh: '今日头条',
-    url: 'https://www.toutiao.com/search/?keyword=%s'
   }
 ]
