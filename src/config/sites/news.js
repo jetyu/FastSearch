2b5804f39592191f37bcac25b79fd80ec93c3429
Jet <2314520+jetyu@users.noreply.github.com>
2026-09-17 08:13:41 +0800

    feat: add new news sources including 路透社, 美联社, 德国之声, and NHK

--- src/config/sites/news.js
+++ src/config/sites/news.js
@@ -13,11 +13,27 @@ export default [
     url: 'https://www.toutiao.com/search/?keyword=%s'
   },
   {
+    nameZh: '路透社',
+    url: 'https://www.reuters.com/site-search/?query=%s'
+  },
+  {
     nameZh: 'CNN',
     url: 'https://edition.cnn.com/search/?q=%s'
   },
   {
     nameZh: 'BBC',
     url: 'https://www.bbc.co.uk/search?q=%s'
+  },
+  {
+    nameZh: '美联社',
+    url: 'https://apnews.com/search?q=%s'
+  },
+  {
+    nameZh: '德国之声',
+    url: 'https://www.dw.com/search/en?searchNavigationId=9097&languageCode=en&item=%s'
+  },
+  {
+    nameZh: 'NHK',
+    url: 'https://www3.nhk.or.jp/nhkworld/en/search/?q=%s'
   }
 ]
